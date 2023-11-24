3f0edb084b81a47f47d3abc5bc744c36a2a27681
anikshajol <anikshajol@gmail.com>
2023-11-24 22:00:23 +0600

    auth created for sign up

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-import { getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import app from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  //   const auth = getAuth();
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  //   registration
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("current suer", currentUser);
+    });
+
+    setLoading(false);
+
+    return () => unsubscribe();
+  }, []);
+
   const authInfo = {
-    name: "shajol",
+    createUser,
+    loading,
+    user,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
