484feff01ff5cb2929f207fec5d885a8bd369734
anikshajol <anikshajol@gmail.com>
2023-12-13 00:59:09 +0600

    server link changed

--- src/Hook/useAxiosPublic.jsx
+++ src/Hook/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "https://ams-server-black.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ams-server-black.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxiosPublic = () => {
--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://ams-server-black.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ams-server-black.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
